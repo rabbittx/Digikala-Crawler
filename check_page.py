6aac778db42ec178387fb75bcb8a3122659507ff
amir habibi <rabbitt1991x@gmail.com>
2024-01-06 05:23:34 +0330

    Everything looks good but needs more optimization

--- check_page.py
+++ check_page.py
@@ -2,35 +2,26 @@ from bs4 import BeautifulSoup
 
 # use this file to find seller info (to do not make spam request)
 
-with open('soucre.html','r',encoding='utf-8') as file :
-    page= file.read()
 
 def has_desired_text(tags,find_text):
     for element in tags:
         if find_text in element.text :
             return element
 
-soup = BeautifulSoup(page,'html.parser')
 
-seller_details = {
-    'seller_name':soup.find('h1',{'class':'text-h5 text-neutral-900 whitespace-nowrap'}).text,
-    'seller_Membership_period':soup.find('div',{'class':'w-full flex flex-col mr-5'}).find('p',{'class':'text-body-2'}).text,
-    'seller_Satisfaction_with_the_goods':soup.find('p',string='رضایت از کالاها').find_parent('div').find('p').text,
-    'Seller_performance':soup.find('p',string='عملکرد فروشنده').find_parent('div').find('p').text,
-    
-}
-
-seller_more_details = {'People have given points':has_desired_text(soup.find_all('p',),'نفر امتیاز داده‌اند').string.replace('نفر امتیاز داده‌اند',''),
-                      'timely supply':soup.find('p', string='تامین به موقع').find_previous_sibling('p').string,
-                      'Obligation to send':soup.find('p', string='تعهد ارسال').find_previous_sibling('p').string,
-                      'No return':soup.find('p', string='بدون مرجوعی').find_previous_sibling('p').string,
-                      'Introduction of the seller':soup.find('span',string='معرفی فروشنده').find_parent('div').find_parent('div').find_next_sibling('div').text,
-                      }
-
-
-
-products = soup.find_all('div',{'class':'product-list_ProductList__item__LiiNI'})
-for product in products:
+def seller_details(soup):
+    return {
+            'seller_name':soup.find('h1',{'class':'text-h5 text-neutral-900 whitespace-nowrap'}).text,
+            'seller_Membership_period':soup.find('div',{'class':'w-full flex flex-col mr-5'}).find('p',{'class':'text-body-2'}).text,
+            'seller_Satisfaction_with_the_goods':soup.find('p',string='رضایت از کالاها').find_parent('div').find('p').text,
+            'Seller_performance':soup.find('p',string='عملکرد فروشنده').find_parent('div').find('p').text,
+            'People_have_given_points':has_desired_text(soup.find_all('p',),'نفر امتیاز داده‌اند').string.replace('نفر امتیاز داده‌اند',''),
+            'timely_supply':soup.find('p', string='تامین به موقع').find_previous_sibling('p').string,
+            'Obligation_to_send':soup.find('p', string='تعهد ارسال').find_previous_sibling('p').string,
+            'No_return':soup.find('p', string='بدون مرجوعی').find_previous_sibling('p').string,
+            'Introduction_of_the_seller':soup.find('span',string='معرفی فروشنده').find_parent('div').find_parent('div').find_next_sibling('div').text if soup.find('span',string='معرفی فروشنده') else 'info unavailable ',
+           }
+def extract_product_details(product):
     img_element = product.find('picture').find('img', {'class': 'w-full rounded-medium inline-block'})
     rate_element = product.find('div',{'class':'mb-1 flex items-center justify-between'}).find('p',{'class':'text-body2-strong text-neutral-700'})
     price_element = product.find('span',{'data-testid':'price-final'})
@@ -41,26 +32,37 @@ for product in products:
     except AttributeError:
         product_special_sale = 'unavailable special sale'
 
-    product_details = {
+    return {
+        
         'product_link': "https://www.digikala.com"+product.find('a')['href'],
         'product_image':img_element['src'] if img_element else 'image not found' ,
         'product_rate':rate_element.text if rate_element else 'rate not found',
         'product_name':product.find('h3').text,
-        'product_price':price_element.text.replace(',','') if price_element else "unavailable",
+        'product_price':price_element.text.replace(',','') if price_element else "product unavailable",
         'product_price_discount_percent':price_discount_percent_element.text if price_discount_percent_element else "unavailable discount percent",
         'product_price_discount':element_price_discount.text if element_price_discount else "unavailable discount price" ,
         'product_special_sale':product_special_sale,
         'stock':has_desired_text(product.find('p'),'باقی مانده').replace('تنها ','').replace(' عدد در انبار باقی مانده','') if has_desired_text(product.find('p'),'باقی مانده') else 'Quantity unspecified',
     }
     
-
-   
-
-
-    
-    
-
-    
+def make_seller_details(seller_detail,product_details):
+     return {
+            'seller_name':seller_detail['seller_name'],
+            'seller_Membership_period':seller_detail['seller_Membership_period'],
+            'seller_Satisfaction_with_the_goods':seller_detail['seller_Satisfaction_with_the_goods'],
+            'Seller_performance':seller_detail['Seller_performance'],
+            'People_have_given_points':seller_detail['People_have_given_points'],
+            'timely_supply':seller_detail['timely_supply'],
+            'Obligation_to_send':seller_detail['Obligation_to_send'],
+            'No_return':seller_detail['No_return'],
+            'Introduction_of_the_seller':seller_detail['Introduction_of_the_seller'],
+            'product_details':product_details,
+           }
+def scan(page_source):
+    soup = BeautifulSoup(page_source,'html.parser')
+    seller_detail = seller_details(soup)
+    product_details = [extract_product_details(product) for product in soup.find_all('div', {'class':'product-list_ProductList__item__LiiNI'})]
+    return make_seller_details(seller_detail,product_details)
 
 
 
--- main.py
+++ main.py
@@ -1,43 +1,44 @@
-from bs4 import BeautifulSoup
-from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from getpass import getpass
 from selenium.webdriver.firefox.service import Service
 from selenium import webdriver
 import time
+from check_page import scan
 geko_path = r'geckodriver.exe'
-base_url = "https://www.digikala.com/seller/a9h3m/"
-
+base_url = ["https://www.digikala.com/seller/a9h3m/",'https://www.digikala.com/seller/cajwj/','https://www.digikala.com/seller/cgxgh/','https://www.digikala.com/seller/cwe4n/','https://www.digikala.com/seller/cskc4/','https://www.digikala.com/seller/f7k6y/']
 
 
 service = Service(geko_path)
 driver = webdriver.Firefox(service=service)
-driver.get(base_url)
-time.sleep(5)
 
-last_height = driver.execute_script("return document.body.scrollHeight")
-
-while True:
-    # اسکرول به انتهای صفحه
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-    # انتظار برای بارگذاری محتوای جدید
+def scan_sellers(url):
+    driver.get(url)
     time.sleep(5)
 
-    # محاسبه ارتفاع جدید و مقایسه با ارتفاع قبلی
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
-        break
-    last_height = new_height
+    last_height = driver.execute_script("return document.body.scrollHeight")
 
-driver.execute_script("window.scrollTo(0, 0);")
+    while True:
+        # اسکرول به انتهای صفحه
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
-details_elemnt = driver.find_element(By.XPATH,"//p[text()='جزئیات بیشتر']/..")
-time.sleep(1)
-details_elemnt.click()
-page_source = driver.page_source
+        # انتظار برای بارگذاری محتوای جدید
+        time.sleep(5)
 
-with open('soucre.html','w',encoding='utf-8') as file :
-    file.write(str(page_source))
+        # محاسبه ارتفاع جدید و مقایسه با ارتفاع قبلی
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+    driver.execute_script("window.scrollTo(0, 0);")
+
+    details_elemnt = driver.find_element(By.XPATH,"//p[text()='جزئیات بیشتر']/..")
+    time.sleep(1)
+    details_elemnt.click()
+    page_source = driver.page_source
+
+    info = scan(page_source)
+    # save info in database 
+    print(info)
+
+for url in base_url:
+    scan_sellers(url)
