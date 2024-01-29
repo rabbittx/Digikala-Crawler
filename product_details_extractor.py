6d99329507c041694a4a15dc4c070a54f434de35
amir habibi <rabbitt1991x@gmail.com>
2024-01-30 03:25:05 +0330

    other seller fixed, check empty data fiexed

--- product_details_extractor.py
+++ product_details_extractor.py
@@ -93,8 +93,7 @@ class ProductDetailsExtractor:
         if isinstance(element ,(Tag, ResultSet)):
             details={}
             details["other_sellers"]= self.safe_extraction('other sellers',element, lambda e: e.find('span',{'data-cro-id':'pdp-other-seller'}).text)
-            if details["other_sellers"] == '' or ' ':
-                details["other_sellers"] = '0'  
+
             details["satisfaction_with_the_product"]= self.safe_extraction('satisfaction with the product',element, lambda e: e.find('div',{"data-cro-id":"pdp-seller-info-cta"}).find('p',{'class':'ml-1 text-body2-strong'}).text)
             details["warranty"]= self.safe_extraction('warranty',element, lambda e: e.find('div',{'data-cro-id':'pdp-shipment-info'}).find_previous('p',{'class':'text-button-2 text-neutral-700'}).text)
             details["digiclub_points"]= self.safe_extraction('digiclub points',element, lambda e: e.find('div',{'data-cro-id':'pdp-shipment-info'}).find_next('p',{'class':'text-button-2 text-neutral-700'}).text)
@@ -319,19 +318,25 @@ class ProductDetailsExtractor:
                     "main_product_details" : main_product_details,
                     "buy_box" : buy_box,
                     "product_image" : product_images,
-                    "other_seller" : other_seller,
-                    "similar_products" : similar_products,
-                    "related_videos" : related_videos,
-                    "introduction_box" : [introduction_box],
-                    "expert_check" : expert_check,
-                    "specifications_box" : specifications_box,
-                    "reviews" : reviews,
-                    "question_box" : question_box,
-                    "also_bought_items" : also_bought_items,
-                    "seller_offer" : seller_offer,
+                    "other_seller" : self.check_not_empity(other_seller),
+                    "similar_products" : self.check_not_empity(similar_products),
+                    "related_videos" : self.check_not_empity(related_videos),
+                    "introduction_box" : self.check_not_empity([introduction_box]),
+                    "expert_check" : self.check_not_empity(expert_check),
+                    "specifications_box" : self.check_not_empity(specifications_box),
+                    "reviews" : self.check_not_empity(reviews),
+                    "question_box" : self.check_not_empity(question_box),
+                    "also_bought_items" : self.check_not_empity(also_bought_items),
+                    "seller_offer" : self.check_not_empity(seller_offer),
             }
         self.db_handler.update_database(data=prodcut_info,column_name='product_id',table_name='products_extraction')
-
+    def check_not_empity(self,data):
+        if isinstance(data,str) and len(data)== 0:
+            return '0'
+        elif isinstance(data,list) and len(data)== 0:
+            return['0']
+        else:
+            return data 
     def run(self,url):          
         self.driver.load_page(url)
         ids = url.split('/')[4]
