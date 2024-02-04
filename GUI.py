c8d69a424c430818a3aa2ff68612b7aec3e9115d
amir habibi <rabbitt1991x@gmail.com>
2024-02-04 07:24:17 +0330

    try to make GUI home page as html css js

---
+++ GUI.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, jsonify
+from panel import WebScraperPanel, ConfigManager  # نام ماژول شما را وارد کنید
+from logger import setup_logger
+app = Flask(__name__)
+log = None
+web_scraper_panel = None
+config_manager = None
+
+
+from flask import Flask, render_template, jsonify
+import time
+
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+log_messages = []
+
+def log_activity(message):
+    timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
+    log_entry = f'[{timestamp}] {message}'
+    log_messages.append(log_entry)
+
+@app.route('/')
+def home():
+    return render_template('pytest.html')
+
+@app.route('/get_logs')
+def get_logs():
+    return jsonify(log_messages)
+
+if __name__ == '__main__':
+    app.run(debug=True)
